740336aaf2a36a51586daf2d41f41a4d67b2184f
hampus <hampus.john.olausson@gmail.com>
2021-10-24 23:23:33 +0200

    Sorted the stories alphabetically

--- src/Components/Button/Button.stories.js
+++ src/Components/Button/Button.stories.js
@@ -2,11 +2,13 @@ import React from 'react'
 import Button from './Button'
 
 export default {
-  title: 'Button',
+  title: '/Form/Button',
   component: Button
 }
 
 export const Primary = () => <Button variant='primary'>Primary</Button>
-export const Secondary = () => <Button variant='secondary'>Primary</Button>
-export const Success = () => <Button variant='success'>Primary</Button>
-export const Danger = () => <Button variant='danger'>Primary</Button>
+export const Secondary = () => <Button variant='secondary'>Secondary</Button>
+export const Success = () => <Button variant='success'>Success</Button>
+export const Danger = () => <Button variant='danger'>Danger</Button>
+
+Primary.storyName = 'The standard Button';
