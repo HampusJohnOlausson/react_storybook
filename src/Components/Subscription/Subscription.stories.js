26f2069432c5533a388f8559b04c7bb46cd160af
hampus <hampus.john.olausson@gmail.com>
2021-10-24 23:30:01 +0200

    Created a story within a story

---
+++ src/Components/Subscription/Subscription.stories.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Primary } from '../Button/Button.stories'
+
+export default {
+  title: 'form/Subscription'
+}
+
+export const PrimarySubscription = () => (
+  <>
+  <Primary/>
+  </>
+)
